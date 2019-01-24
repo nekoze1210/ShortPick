1fdbd0eb6d2737c35f8c7a65c06d5165e2f90839
Daiki Nagaoka <neko.z.n1210@gmail.com>
2019-01-24 23:20:42 +0900

    remove debug code

--- src/api/request/base.js
+++ src/api/request/base.js
@@ -5,11 +5,9 @@ exports.post = function(url, jsonOptions) {
   axios
     .post(url, jsonOptions)
     .then(response => {
-      console.log('SUCCEEDED: Sent slack webhook: \n', response.data)
       return response
     })
     .catch(error => {
-      console.log('FAILED: Send slack webhook', error)
       dialog.showErrorBox('ConnectionError', 'Message:' + error)
       return error
     })
